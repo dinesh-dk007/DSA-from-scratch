ab25d4f9f72219be728a1cc07552324481e212de
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-07-03 21:49:45 +0530

    Day 4

---
+++ String/3304 - Find the K-th Character in the String Game l.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder sb = new StringBuilder("a");
+        while(sb.length() < k){
+            int n = sb.length();
+            for(int i = 0;i<n;i++){
+                sb.append((char) ('a' + ((sb.charAt(i) - 'a') + 1) % 26));
+            }
+        }
+        //String str = sb.toString();
+        return sb.charAt(k-1);
+    }
+}
