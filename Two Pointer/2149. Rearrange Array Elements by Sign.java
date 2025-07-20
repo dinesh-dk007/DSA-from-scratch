8a755a036d6d02d76c203b84f0bbc1e88e965f71
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-07-20 09:20:02 +0530

    Day 16

---
+++ Two Pointer/2149. Rearrange Array Elements by Sign.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int [n];
+        int i = 0;
+        int j = 1;
+        for(int k = 0;k<n;k++){
+            if(nums[k] > 0){
+                arr[i] = nums[k];
+                i += 2;
+            }else{
+                arr[j] = nums[k];
+                j += 2;
+            }
+        }
+        return arr;
+    }
+}
