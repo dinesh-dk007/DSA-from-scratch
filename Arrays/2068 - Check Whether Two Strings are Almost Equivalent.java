4a9618bf1a515112779e2fdae0c4a68f7a2cd78a
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-09-28 09:09:17 +0530

    Day 67

---
+++ Arrays/2068 - Check Whether Two Strings are Almost Equivalent.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int [] arr = new int [26];
+        for(char c : word1.toCharArray()){
+            arr[c-'a']++;
+        }
+        for(char c : word2.toCharArray()){
+            arr[c-'a']--;
+        }
+        for(int i : arr){
+            if(i > 3 || i < -3)return false;
+        }
+        return true;
+
+    }
+}
