ffdbdd9d2ff561cabc08f988cd5eb549d74da90e
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-11-01 08:23:48 +0530

    Day 81

---
+++ Math/3726 - Remove Zeros in Decimal Representation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long removeZeros(long n) {
+        String str = Long.toString(n);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != '0')
+                sb.append(str.charAt(i));
+        }
+        return Long.parseLong(sb.toString());
+    }
+}
