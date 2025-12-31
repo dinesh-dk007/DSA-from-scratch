74926118742fa07083dc34eb3c75735998be49ee
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2025-12-31 19:02:23 +0530

    Day 94

--- DP/1143 - Longest Common Subsequence.java
+++ DP/1143 - Longest Common Subsequence.java
@@ -1,18 +1,20 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();int m = text2.length();
-        int [][] arr = new int [n+1][m+1];
-        for(int i = 0;i<=n;i++){
-            for(int j = 0;j<=m;j++){
-                if(i == 0 || j == 0){
-                    arr[i][j] = 0;
-                }else if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    arr[i][j] = arr[i-1][j-1]+1;
+        int n = text1.length(); int m = text2.length();
+        int []prev = new int [m+1];
+        Arrays.fill(prev,0);
+        for(int i = 1;i<=n;i++){
+            int [] curr = new int [m+1];
+            curr[0] = 0;
+            for(int j = 1;j<=m;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    curr[j] = 1 + prev[j-1];
                 }else{
-                    arr[i][j] = Math.max(arr[i-1][j],arr[i][j-1]);
+                    curr[j] = Math.max(curr[j-1],prev[j]);
                 }
             }
+            prev = curr.clone();
         }
-        return arr[n][m];
+        return prev[m];
     }
 }
