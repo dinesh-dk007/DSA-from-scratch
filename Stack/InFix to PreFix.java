80048e45da9bd35ce849a62323970744f8fec4fa
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2026-01-15 11:44:42 +0530

    Day 104

---
+++ Stack/InFix to PreFix.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String inFix = "a+b*(c^d-e)";
+      String new_inFix = new StringBuilder(inFix).reverse().toString();
+      Stack<Character> stk = new Stack<>();
+      StringBuilder sb = new StringBuilder();
+      Map<Character,Integer> map = new HashMap<>();
+      map.put('^',1);
+      map.put('*',2);
+      map.put('/',2);
+      map.put('+',3);
+      map.put('-',3);
+      
+      for(char c : new_inFix.toCharArray()){
+        if(Character.isLetterOrDigit(c)){
+          
+            sb.append(c);
+        }
+        else if(c == ')'){
+          
+            stk.push(c);
+            
+        }else if(c == '('){
+          
+            while(!stk.isEmpty() && stk.peek() != ')'){
+              sb.append(stk.pop());
+            }
+            stk.pop();
+            
+        }else{
+            while(!stk.isEmpty() && stk.peek() != ')' &&
+      (map.get(stk.peek()) < map.get(c) ||
+      (map.get(stk.peek()) == map.get(c) && c != '^'))){
+              sb.append(stk.pop());
+            }
+            stk.push(c);
+        }
+      }
+      while(!stk.isEmpty()){
+        
+        sb.append(stk.pop());
+        
+      }
+      System.out.print("PreFix: " + sb.reverse().toString());
+      
+  }
+  
+}
