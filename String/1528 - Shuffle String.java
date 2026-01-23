63735f2d50d3013342c9a8cd31b7323b16ea6e9a
dinesh-dk007 <dineshkumaar9025067808@gmail.com>
2026-01-23 20:21:48 +0530

    Day 112

---
+++ String/1528 - Shuffle String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char [] arr = new char[s.length()];
+        for(int i = 0;i<indices.length;i++){
+            char c = s.charAt(i);
+            arr[indices[i]] = c;
+        }
+        return new String(arr);
+    }
+}
